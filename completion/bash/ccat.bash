22e421ac4ea2dd352a35305bd7a6689614c1e267
Phate Valleyman <Jonas.Ned@outlook.com>
2023-10-16 16:40:42 +0200

    modified:   completion/bash/ccat.bash

--- completion/bash/ccat.bash
+++ completion/bash/ccat.bash
@@ -1,27 +1,40 @@
-_ccat()
-{
-	local cur prev words cword
-	_init_completion || return
+# Bash completion for ccat
 
-	local -a ccat_options=(
-		'--bg='
-		'-C'
-		'--color='
-		'-G'
-		'--color-code='
-		'-h'
-		'--help'
-		'--html='
-		'--palette='
-		'-v'
-		'*:filename:_files'
-	)
+_ccat() {
+  local cur prev opts
+  COMPREPLY=()
+  cur="${COMP_WORDS[COMP_CWORD]}"
+  prev="${COMP_WORDS[COMP_CWORD-1]}"
+  opts="--bg --color -C --color-code -G -h --help --html --palette -v --version"
 
-	if [[ "${prev}" == -* ]]; then
-		COMPREPLY=( $(compgen -W "${ccat_options[*]}" -- ${cur}) )
-	else
-		COMPREPLY=( $(compgen -f -- ${cur}) )
-	fi
+  case "${prev}" in
+    --bg)
+      COMPREPLY=($(compgen -W "light dark" -- ${cur}))
+      return 0
+      ;;
+    -C | --color)
+      COMPREPLY=($(compgen -W "never always auto" -- ${cur}))
+      return 0
+      ;;
+    -G | --color-code)
+      return 0
+      ;;
+    -h | --help)
+      return 0
+      ;;
+    --html)
+      return 0
+      ;;
+    --palette)
+      return 0
+      ;;
+    -v | --version)
+      return 0
+      ;;
+    *)
+      COMPREPLY=($(compgen -W "${opts}" -- ${cur}))
+      ;;
+  esac
 }
 
 complete -F _ccat ccat
